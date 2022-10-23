f0b9c1fa2f090d46cc1ffa9e93b51788cc1fe65d
martin1952patrick <martinpati1952@gmail.com>
2022-10-23 23:15:55 +0300

    add java and typescript

--- java/Main.java
+++ java/Main.java
@@ -5,13 +5,18 @@ class Main{
     public static void main(String args[]){
         //main mainly for having input values to be testes
         Integer[] arr = {2,6,8,3,5,4,7};
-        martinCode(arr);
+        find_total(arr);
     }
     
-public static Integer martinCode(Integer[] my_list){
+    //Integer [] find_total = new Integer[my_list];
+    //find_total(arr);
+public static Integer find_total(Integer[] my_list){
+
+    
 
     //Initialize the variables and give the even and odd zero '0' value to mean the 
     // initial count/summation start from '0' 
+    
 
         int evens = 0;
 
@@ -37,4 +42,42 @@ public static Integer martinCode(Integer[] my_list){
         return evens - odds;
     }
 }
+
+// different part but same question
+
+
+
+
+public static Integer find_total( Integer[] my_list ) {
+    //Insert your code here 
+
+
+     //Initialize the variables and give the even and odd zero '0' value to mean the 
+    // initial count/summation start from '0' 
+    
+      int evens = 0;
+
+       int odds = 0;
+
+       for(int num =0;num<my_list.length;num++){
+
+           //we chech if the number index is  even or odd, so we set condition for checking
+           //even and the other will automatically be odd if the first condition of being
+           //even is not met, during checking the evens are added and odd are also added 
+
+           if(num%2==0){
+               evens+=my_list[num];
+           }
+           else{
+               odds+=my_list[num];
+           }
+       }
+       // we now return the differeces in the total of evens and odd
+       System.out.println(evens-odds);
+
+       return evens - odds;
+
+    
+}
+
      
--- type/type.ts
+++ type/type.ts
@@ -1,13 +1,3 @@
-//typescript program to find minimum difference in an inputArray
-// of maximum A and minimum B in two subsequences.
-
- // Return minimum difference of
- // maximum A and minimum B of 
- // elements in two two sequences
-
- 
-   //const inputArray = [1, 3, 10, 4, 19, 5, 7, 9, 15];
-//export default function minDif(num:number,
 const inputArray = [1, 5, 7, 9, 15]
 
 let minDif;
@@ -17,7 +7,7 @@ let A;
 let B;
 
 // Loop and find minimum value
-for (i = 0; i < inputArray.length - 1; i++) {
+for (let i = 0; i < inputArray.length - 1; i++) {
     //find |max(A) - min(B)|
   let dif = Math.abs(
     Math.max(...inputArray.slice(0, i + 1)) - Math.min(...inputArray.slice(i + 1))
